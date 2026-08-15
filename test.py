2373e80191a7eb3b328e2768fdeb9308a1f38bbc
ropocky <ropocky@gmail.com>
2026-08-15 15:28:32 +0800

    none

--- test.py
+++ test.py
@@ -1,1 +1,31 @@
-print("hello world")
+import ipaddress
+cnip = []
+with open(r'china_ip_list.txt','r') as r:
+    for line in r:
+        line = line.strip()
+        if not line:
+            continue
+        ip = ipaddress.ip_network(line)
+        cnip.append(ip)
+result = list(ipaddress.collapse_addresses(cnip))
+
+print("原始数量:", len(cnip))
+print("合并后数量:", len(result))
+
+class Node :
+    def __init__(self,ip):
+        self.ip = ip
+        self.left = None
+        self.right = None
+        self.is_cn = False
+    def split(self):
+        subnet = list(
+            self.ip.subnets(prefixlen_diff=1)
+        )
+        self.left = Node(subnet[0])
+        self.right = Node(subnet[1])
+def insert_cn(node,cn)
+    if node.network == cn:
+        node.is_cn = True
+        return
+    if not node.network
