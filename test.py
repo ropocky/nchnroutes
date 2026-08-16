b778cf91c856921ed4a3e7f22dd547c25d3d09f6
ropocky <ropocky@gmail.com>
2026-08-17 00:43:38 +0800

    none

--- test.py
+++ test.py
@@ -32,22 +32,15 @@ def insert_cn(node,cn)
         return
     if not node.left is None:
         node.split()
-
     insert_cn(node.left,cn)
     insert_cn(node.right,cn)
 
 def dump_non_cn(node, result):
-
-    # 中国IP不输出
     if node.is_cn:
         return
-
-    # 到达叶子节点
     if node.left is None:
-
         result.append(node.network)
         return
-
     dump_non_cn(node.left, result)
     dump_non_cn(node.right, result)
 
