3eea2b8286ba76d9e09f4e94cd45cfd04a60d247
ropocky <ropocky@gmail.com>
2026-08-17 22:00:52 +0800

    none

--- test.py
+++ test.py
@@ -1,55 +1,61 @@
 import ipaddress
 
-class Node :
-    def __init__(self,ip):
-        self.ip = ip
+class Node:
+    def __init__(self, ip):
+        self.ip = ipaddress.ip_network(ip) if isinstance(ip, str) else ip
         self.left = None
         self.right = None
         self.is_cn = False
+
     def split(self):
-        subnet = list(
-            self.ip.subnets(prefixlen_diff=1)
-        )
-        self.left = Node(subnet[0])
-        self.right = Node(subnet[1])
-def insert_cn(node,cn):
-    if node.network == cn:
+        subnets = list(self.ip.subnets(prefixlen_diff=1))
+        self.left = Node(subnets[0])
+        self.right = Node(subnets[1])
+
+def insert_cn(node, cn):
+    if node.ip == cn:
         node.is_cn = True
         return
-    if not node.network.overlaps(cn):
+    if not node.ip.overlaps(cn):
         return
-    if not node.left is None:
+    if node.left is None:
         node.split()
-    insert_cn(node.left,cn)
-    insert_cn(node.right,cn)
+    insert_cn(node.left, cn)
+    insert_cn(node.right, cn)
 
 def dump_non_cn(node, result):
     if node.is_cn:
         return
     if node.left is None:
-        result.append(node.network)
+        result.append(node.ip)
         return
     dump_non_cn(node.left, result)
     dump_non_cn(node.right, result)
 
 def load_file(filename):
-    cnip = []
-    with open(filename,'r') as r:
+    cn_list = []
+    with open(filename, 'r') as r:
         for line in r:
             line = line.strip()
             if not line:
                 continue
             ip = ipaddress.ip_network(line)
-            cnip.append(ip)
-    return cnip
+            cn_list.append(ip)
+    return cn_list
 
 def main():
-    cn_list = load_file(r'china_ip_list.txt')
-    root = Node(r'0.0.0.0/0')
+    cn_list = load_file('china_ip_list.txt')
+    root = Node('0.0.0.0/0')
     for cn in cn_list:
-        insert_cn(root,cn)
+        insert_cn(root, cn)
 
-    result = list()
+    result = []
+    dump_non_cn(root, result)
+    print(len(result))
+    with open(r'non-cn.txt', 'w') as f:
+        for network in result:
+            f.write(str(network) + '\n')
+    print('完成')
 
-    dump_non_cn(root,result)
-
+if __name__ == '__main__':
+    main()
